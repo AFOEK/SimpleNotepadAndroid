9f99f30ec3c8d54f8c0da82220fb922b79b1b845
AFOEK <felix.montalfu@gmail.com>
2020-10-23 15:53:59 +0700

    Version 0.0.1

---
+++ app/src/main/java/NoteBuilder.java
@@ -0,0 +1,19 @@
+public class NoteBuilder {
+    private String title, content;
+
+    public NoteBuilder() {
+    }
+
+    public NoteBuilder(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
